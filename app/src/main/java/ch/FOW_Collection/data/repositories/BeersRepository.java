13215119075e184864c213ec07258696791f46a6
Gachnang <markus.gachnang@gmx.ch>
2018-10-17 13:04:03 +0200

    Change Namespace from "beerpro" to "FOW_Collection"

---
+++ app/src/main/java/ch/FOW_Collection/data/repositories/BeersRepository.java
@@ -0,0 +1,63 @@
+package ch.FOW_Collection.data.repositories;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import ch.FOW_Collection.domain.models.Beer;
+import ch.FOW_Collection.domain.utils.FirestoreQueryLiveData;
+import ch.FOW_Collection.domain.utils.FirestoreQueryLiveDataArray;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static androidx.lifecycle.Transformations.map;
+import static androidx.lifecycle.Transformations.switchMap;
+
+public class BeersRepository {
+
+    private final static Function<List<Beer>, List<String>> mapBeersToCategories = (List<Beer> beers) -> {
+        Set<String> filtered = new HashSet<>();
+        for (Beer beer : beers) {
+            filtered.add(beer.getCategory());
+        }
+        String[] strings = filtered.toArray(new String[0]);
+        return Arrays.asList(strings).subList(0, Math.min(8, strings.length));
+    };
+
+    private final static Function<List<Beer>, List<String>> mapBeersToManufacturers = (List<Beer> beers) -> {
+        Set<String> filtered = new HashSet<>();
+        for (Beer beer : beers) {
+            filtered.add(beer.getManufacturer());
+        }
+        String[] strings = filtered.toArray(new String[0]);
+        Arrays.sort(strings);
+        return Arrays.asList(strings);
+    };
+
+    private final FirestoreQueryLiveDataArray<Beer> allBeers =
+            new FirestoreQueryLiveDataArray<>(FirebaseFirestore.getInstance().collection(Beer.COLLECTION), Beer.class);
+
+    private static LiveData<Beer> getBeerById(String beerId) {
+        return new FirestoreQueryLiveData<>(
+                FirebaseFirestore.getInstance().collection(Beer.COLLECTION).document(beerId), Beer.class);
+    }
+
+    public LiveData<List<Beer>> getAllBeers() {
+        return allBeers;
+    }
+
+    public LiveData<Beer> getBeer(LiveData<String> beerId) {
+        return switchMap(beerId, BeersRepository::getBeerById);
+    }
+
+    public LiveData<List<String>> getBeerCategories() {
+        return map(allBeers, mapBeersToCategories);
+    }
+
+    public LiveData<List<String>> getBeerManufacturers() {
+        return map(allBeers, mapBeersToManufacturers);
+    }
+
+}
---
+++ app/src/main/java/ch/FOW_Collection/presentation/explore/ExploreFragment.java
@@ -0,0 +1,81 @@
+package ch.FOW_Collection.presentation.explore;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ch.FOW_Collection.R;
+import ch.FOW_Collection.presentation.explore.search.SearchActivity;
+import ch.FOW_Collection.presentation.utils.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+/**
+ * This fragment is the first fragment shown in the {@link ch.FOW_Collection.presentation.MainActivity}. It lets users
+ * "discover" beers by implementing a search (which simply opens the {@link SearchActivity} and two nested tabs where
+ * users can browse beer categories and breweries.
+ */
+public class ExploreFragment extends Fragment {
+
+    private static final String TAG = "HomeScreenActSearch";
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+
+    @BindView(R.id.tablayout)
+    TabLayout tabLayout;
+
+    /**
+     * Fragments all need to have an empty constructor because the system might have the instantiate them.
+     */
+    public ExploreFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        View rootView = inflater.inflate(R.layout.fragment_explore, container, false);
+        ButterKnife.bind(this, rootView);
+
+        /*
+         * Because the two fragments are nested in this fragment, we have to use the getChildFragmentManager.
+         * Otherwise strange rendering and other bugs can occur at runtime.
+         *
+         * The two fragments will communicate directly with the MainActivity (that's why the MainActivity implements
+         * two listener interfaces), bypassing this fragment.
+         * */
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new BeerCategoriesFragment(), "Bierart");
+        adapter.addFragment(new BeerManufacturersFragment(), "Brauerei");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return rootView;
+    }
+
+    /**
+     * This method is called when the user clicks the beerSearchButton in fragment_explore. We also use ButterKnife's
+     * {@link OnClick} annotation to keep the keep the code shorter.
+     * <p>
+     * When the activity is started, the shared element (the animationSource) will be animated. It's not actually a
+     * shared element in the sense that both layouts somehow share the same instance, we can simply designate two
+     * elements as shared by giving both the same sharedElementName, i.e., "search" in this case.
+     * <p>
+     * See <a href="https://guides.codepath.com/android/shared-element-activity-transition">this blog</a> for details
+     * on how it works.
+     *
+     * @param animationSource
+     */
+    @OnClick(R.id.beerSearchButton)
+    public void openSearchActivity(View animationSource) {
+        Intent intent = new Intent(getActivity(), SearchActivity.class);
+        ActivityOptions options =
+                ActivityOptions.makeSceneTransitionAnimation(getActivity(), animationSource, "search");
+        startActivity(intent, options.toBundle());
+    }
+}
